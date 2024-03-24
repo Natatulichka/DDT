61fbdf09243cc50d3ca2da3024e93c34615ac7df
Sergey Stupak <itstupak@gmail.com>
2024-03-24 03:43:07 +0100

    add bg-image for popup menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,5 +1,16 @@
-$(document).ready(function() {
+// Код не работал :(
+    
+/* $(document).ready(function() {
     $('burger-btn').click(function (event) {
         $('header-popup-menu').addClass('is-open');
     });
-});
+}); */
+
+
+// Вторая попытка - код не работает :(
+
+/* document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("burger").addEventListener("click", function() {
+        document.querySelector("header-popup-menu").classList.add("is-open")
+    })
+}) */
