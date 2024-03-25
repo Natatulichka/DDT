c2a0bd05d87f91b3a8e9174f7bc817e07ecd5b22
Sergey Stupak <itstupak@gmail.com>
2024-03-25 13:15:45 +0100

    fixed burger.js

--- src/js/burger.js
+++ src/js/burger.js
@@ -2,9 +2,9 @@
 let menuBtn = document.querySelector('.burger-btn');
 let menu = document.querySelector('.header-popup-menu');
 menuBtn.addEventListener('click', function(){
-	menu.classList.add('open');
+	menu.classList.add('is-open');
 })
 let closeBtn = document.querySelector('.close-btn');;
 closeBtn.addEventListener('click', function(){
-	menu.classList.remove('open');
+	menu.classList.remove('is-open');
 })
