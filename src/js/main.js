2722c7ee369f6818313fbefaeb765744c040b1aa
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-03-26 09:58:38 +0100

    js

--- src/js/main.js
+++ src/js/main.js
@@ -43,6 +43,7 @@
                    link.addEventListener('click', function (e) {
                         e.preventDefault();
                         const currentTarget = this.getAttribute('href');
+                        refs.mobilemenu.classList.contains("is-open") ? refs.mobilemenu.classList.remove("is-open") : null;
                         smoothScroll(currentTarget, 1000);
                    });
               });
